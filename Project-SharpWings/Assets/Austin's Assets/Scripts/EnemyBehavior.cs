475ad6146bad423252d5b8c350a15424cc493e9a
folsterA <folsteraustin@gmail.com>
2022-04-20 22:47:40 -0700

    Enemy Pathing + Swarming

--- Project-SharpWings/Assets/Austin's Assets/Scripts/EnemyBehavior.cs
+++ Project-SharpWings/Assets/Austin's Assets/Scripts/EnemyBehavior.cs
@@ -1,31 +1,67 @@
+using System;
 using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    [HideInInspector]
     public GameObject target;
+
+    [HideInInspector]
     public GameObject groupParent;
 
-    [SerializeField] private GameObject gunPosition;
-    [SerializeField] private float fireRate;
-    [SerializeField] private float minTargetDistance, maxTargetDistance;
+    [HideInInspector]
+    public float fireRate, fireRateOffset;
+    private float _fireTimer;
+    [HideInInspector]
+    public float minTargetDistance, maxTargetDistance;
+    
+    [HideInInspector]
+    public string currentState;
+    
+    // swarming stuff
+    [HideInInspector] 
+    public float swarmRadius, rotationSpeed;
+    [HideInInspector]
+    public Vector3 rotationAxis;
 
-    private float _distanceToTarget;
+    private void Start()
+    {
+        fireRate += fireRateOffset;
+        var center = groupParent.transform.position;
+        transform.position = (transform.position - center).normalized * swarmRadius + center;
+    }
 
     private void Update()
     {
-        var targetTransform = target.transform;
-        _distanceToTarget = (targetTransform.position - transform.position).magnitude;
+        var thisPosition = transform.position;
         
-        // if you're not within targeting distance, look towards the path and exit
-        if (!(_distanceToTarget > minTargetDistance || _distanceToTarget < maxTargetDistance))
+        // swarm around group parent
+        var center = groupParent.transform.position;
+        transform.RotateAround(center, rotationAxis, rotationSpeed * Time.deltaTime);
+        var desiredPosition = (thisPosition - center).normalized * swarmRadius + center;
+        thisPosition = desiredPosition;
+
+        // check for range when pathing
+        var targetPosition = target.transform.position;
+        var toTarget = targetPosition - thisPosition;
+        var distanceToTarget = toTarget.magnitude;
+        var dot = Vector3.Dot(targetPosition, toTarget);
+        
+        if (currentState == "Pathing" && (distanceToTarget < minTargetDistance || distanceToTarget > maxTargetDistance) && dot < 0)
         { 
             transform.LookAt(groupParent.transform.position + groupParent.transform.forward);
+            _fireTimer = Time.time;
             return;
         }
-
+        
         // look at player
-        transform.LookAt(targetTransform.position);
+        transform.LookAt(targetPosition);
         
         // fire
+        if (Time.time - _fireTimer > fireRate)
+        {
+            _fireTimer = Time.time;
+            //Debug.Log("Fire");
+        }
     }
 }
--- Project-SharpWings/Assets/Austin's Assets/Scripts/EnemyGroupBehavior.cs
+++ Project-SharpWings/Assets/Austin's Assets/Scripts/EnemyGroupBehavior.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Cinemachine;
 using UnityEngine;
 
@@ -8,19 +9,27 @@ public class EnemyGroupBehavior : MonoBehaviour
     [SerializeField] private CinemachineSmoothPath trackPath;
 
     [Header("Enemy Information")]
-    [SerializeField] private List<GameObject> enemies;
-    [SerializeField] private float enemySpeed;
+    [SerializeField] private List<EnemyBehavior> enemies;
+    [SerializeField] private float enemyGroupSpeed;
+    [SerializeField] private float enemyFireRate, randFireOffset;
+    [SerializeField] private float minTargetDistance, maxTargetDistance;
 
     [Header("Automatic Path Generation")]
     [SerializeField] private bool autoGeneratePath = true;
     [SerializeField] private float randomCoordOffset;
     [SerializeField] private float minDistanceOffset, maxDistanceOffset;
     [SerializeField] private float targetTrackOffset;
-    [SerializeField] private float swoopDistance;
+
+    [Header("Swarming Settings")] 
+    [SerializeField] private float swarmRadius;
+    [SerializeField] private float rotationSpeed;
+
 
     private CinemachineDollyCart _dollyCart;
     private GameObject _trackedObject;
-    private bool _begunSwoop;
+    private bool _begunPathing;
+
+    private string _currentState;
     
     private void Awake()
     {
@@ -29,72 +38,105 @@ public class EnemyGroupBehavior : MonoBehaviour
 
     private void Start()
     {
-        foreach (var enemy in enemies) enemy.SetActive(false);
-        _begunSwoop = false;
+        foreach (var enemy in enemies) enemy.gameObject.SetActive(false);
+        _currentState = "none";
+        _dollyCart.enabled = false;
     }
 
     private void Update()
     {
-        // if you've reached the end, stop
-        if (_dollyCart.m_Position >= trackPath.PathLength) return;
-
-        // if the enemy group reaches the swoopDistance, it stops tracking the player and begins it's swoop
-        if ((_trackedObject.transform.position - transform.position).magnitude < swoopDistance && !_begunSwoop)
+        switch (_currentState)
         {
-            var newTrackWaypoints = new CinemachineSmoothPath.Waypoint[3];
-            newTrackWaypoints[0] = trackPath.m_Waypoints[0];
-            newTrackWaypoints[1] = trackPath.m_Waypoints[1];
+            case "Pathing":
+                // if you've reached the end, stop and disable enemies
+                if (_dollyCart.m_Position >= trackPath.PathLength)
+                { 
+                    gameObject.SetActive(false);
+                    foreach (var enemyObj in enemies) enemyObj.gameObject.SetActive(false);
+                    _currentState = "none";
+                }
+                break;
             
-            _begunSwoop = true;
-            
-            // set the last waypoint in front of the tracked object
-            var forward = _trackedObject.transform.forward;
-            var positionForwardTracked = forward * Random.Range(minDistanceOffset, maxDistanceOffset);
-            positionForwardTracked.x += Random.Range(-randomCoordOffset, randomCoordOffset);
-            positionForwardTracked.y += Random.Range(-randomCoordOffset, randomCoordOffset);
-            positionForwardTracked.z += Random.Range(-randomCoordOffset, randomCoordOffset);
-
-            newTrackWaypoints[2].position = positionForwardTracked + _trackedObject.transform.position +
-                                            (forward * targetTrackOffset);
-
-            trackPath.m_Waypoints = newTrackWaypoints;
+            case "Swarming":
+                // if there are no longer any enemies, destroy self
+                if (!enemies.Any()) Destroy(gameObject);
+                transform.position = _trackedObject.transform.position;
+                break;
         }
-        else
-        {
-            // set the path's next waypoint to the target
-            trackPath.m_Waypoints[1].position = _trackedObject.transform.position 
-                                                + (_trackedObject.transform.forward * targetTrackOffset);
-        }
+        
     }
 
+    // This methods takes in an object to track and fires or swarms 
     public void BeginPathing(GameObject obj)
     {
         _trackedObject = obj;
+        _currentState = "Pathing";
         
         if (autoGeneratePath)
         {
             // put the first waypoint behind the tracked object
             var forward = _trackedObject.transform.forward;
+            var position = _trackedObject.transform.position;
+            
             var positionBehindTracked = -forward * Random.Range(minDistanceOffset, maxDistanceOffset);
             positionBehindTracked.x += Random.Range(-randomCoordOffset, randomCoordOffset);
             positionBehindTracked.y += Random.Range(-randomCoordOffset, randomCoordOffset);
             positionBehindTracked.z += Random.Range(-randomCoordOffset, randomCoordOffset);
 
-
-            trackPath.m_Waypoints = new CinemachineSmoothPath.Waypoint[2];
-            var position = _trackedObject.transform.position;
-            trackPath.m_Waypoints[0].position = positionBehindTracked + position;
-            trackPath.m_Waypoints[1].position = position
-                                                + (forward * targetTrackOffset);
+            // set the last waypoint in front of the tracked object
+            var positionForwardTracked = forward * Random.Range(minDistanceOffset, maxDistanceOffset);
+            positionForwardTracked.x += Random.Range(-randomCoordOffset, randomCoordOffset);
+            positionForwardTracked.y += Random.Range(-randomCoordOffset, randomCoordOffset);
+            positionForwardTracked.z += Random.Range(-randomCoordOffset, randomCoordOffset);
+            
+            trackPath.m_Waypoints = new CinemachineSmoothPath.Waypoint[3];
+            trackPath.m_Waypoints[0].position = positionBehindTracked + position + (forward * targetTrackOffset);
+            trackPath.m_Waypoints[1].position = position + (forward * targetTrackOffset);
+            trackPath.m_Waypoints[2].position = positionForwardTracked + position + (forward * targetTrackOffset);
         }
         
-        // activate the enemies and give them the tracked object as a target
+        // activate the enemies and give them the data they need
         foreach (var enemy in enemies)
         {
-            enemy.SetActive(true);
-            enemy.GetComponent<EnemyBehavior>().target = _trackedObject;
+            enemy.gameObject.SetActive(true);
+            enemy.target = _trackedObject;
+            enemy.minTargetDistance = minTargetDistance;
+            enemy.maxTargetDistance = maxTargetDistance;
+            enemy.fireRate = enemyFireRate;
+            enemy.fireRateOffset = Random.Range(0, randFireOffset);
+            enemy.groupParent = gameObject;
+            enemy.currentState = _currentState;
+
+            enemy.swarmRadius = 1;
+            enemy.rotationAxis = Random.insideUnitSphere;
+            enemy.rotationSpeed = 60;
+        }
+
+        _dollyCart.m_Path = trackPath;
+        _dollyCart.m_Speed = enemyGroupSpeed;
+        _dollyCart.enabled = true;
+    }
+
+    public void BeginSwarming(GameObject obj)
+    {
+        _trackedObject = obj;
+        _currentState = "Swarming";
+        
+        foreach (var enemy in enemies)
+        {
+            enemy.gameObject.SetActive(true);
+            enemy.target = _trackedObject;
+            enemy.minTargetDistance = minTargetDistance;
+            enemy.maxTargetDistance = maxTargetDistance;
+            enemy.fireRate = enemyFireRate;
+            enemy.fireRateOffset = Random.Range(0, randFireOffset);
+            enemy.groupParent = gameObject;
+            enemy.currentState = _currentState;
+
+            enemy.swarmRadius = swarmRadius;
+            enemy.rotationAxis = Random.insideUnitSphere;
+            enemy.rotationSpeed = rotationSpeed;
         }
         
-        _dollyCart.m_Speed = enemySpeed;
     }
 }
