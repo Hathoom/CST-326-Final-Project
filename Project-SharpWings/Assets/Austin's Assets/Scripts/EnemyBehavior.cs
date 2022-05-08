1e87301e0687b87cf270880195d3d726b1fa6288
tmKammerer <71908549+tmKammerer@users.noreply.github.com>
2022-05-07 17:45:59 -0700

    scripts for certain actions with powerups

---
+++ Project-SharpWings/Assets/Austin's Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    [HideInInspector]
+    public GameObject target;
+
+    [HideInInspector]
+    public GameObject groupParent;
+
+    [HideInInspector]
+    public float fireRate, fireRateOffset;
+    private float _fireTimer;
+    [HideInInspector]
+    public float minTargetDistance, maxTargetDistance;
+    
+    [HideInInspector]
+    public string currentState;
+    
+    // swarming stuff
+    [HideInInspector] 
+    public float swarmRadius, rotationSpeed;
+    [HideInInspector]
+    public Vector3 rotationAxis;
+
+    private void Start()
+    {
+        fireRate += fireRateOffset;
+        fireRate = Time.time;
+        var center = groupParent.transform.position;
+        transform.position = (transform.position - center).normalized * swarmRadius + center;
+    }
+
+    private void Update()
+    {
+        var thisPosition = transform.position;
+        
+        // swarm around group parent
+        var center = groupParent.transform.position;
+        transform.RotateAround(center, rotationAxis, rotationSpeed * Time.deltaTime);
+        var desiredPosition = (thisPosition - center).normalized * swarmRadius + center;
+        thisPosition = desiredPosition;
+
+        // check for range when pathing
+        var targetPosition = target.transform.position;
+        var toTarget = targetPosition - thisPosition;
+        var distanceToTarget = toTarget.magnitude;
+        var dot = Vector3.Dot(targetPosition, toTarget);
+        
+        if (currentState == "Pathing" && (distanceToTarget < minTargetDistance || distanceToTarget > maxTargetDistance || dot < 0))
+        { 
+            transform.LookAt(groupParent.transform.position + groupParent.transform.forward);
+            _fireTimer = Time.time;
+            return;
+        }
+        
+        // look at player
+        transform.LookAt(targetPosition);
+        
+        // fire
+        if (Time.time - _fireTimer > fireRate)
+        {
+            _fireTimer = Time.time;
+            //Debug.Log("Fire");
+        }
+    }
+}
---
+++ Project-SharpWings/Assets/Dylan/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public Vector2 limits = new Vector2(5, 3);
+    private Vector2 movement;
+    private InputManager _inputManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _inputManager = InputManager.createInstance();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayer();
+    }
+    private void movePlayer()
+    {
+        movement = _inputManager.getPlayerMovement();
+        transform.localPosition += new Vector3(movement.x, movement.y, 0f) * speed * Time.deltaTime;
+        clampPosition();
+    }
+
+    private void clampPosition()
+    {
+        Vector3 playerPos = Camera.main.WorldToViewportPoint(transform.position);
+        playerPos.x = Mathf.Clamp01(playerPos.x);
+        playerPos.y = Mathf.Clamp01(playerPos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(playerPos);
+    }
+}
